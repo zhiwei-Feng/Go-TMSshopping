21d8dba345e240c6130688f1816dc504ca0a55db
fengzw <zhiwei.feng1995@gmail.com>
2021-03-24 21:30:13 +0800

    add User domain

---
+++ domain/user.go
@@ -0,0 +1,17 @@
+package domain
+
+type User struct {
+	Id       int    `gorm:"primaryKey;column:EU_USER_ID"`
+	UserName string `gorm:"column:EU_USER_NAME"`
+	Password string `gorm:"column:EU_PASSWORD"`
+	Sex      string `gorm:"column:EU_SEX"`
+	Birthday string `gorm:"column:EU_BIRTHDAY"`
+	Email    string `gorm:"column:EU_EMAIL"`
+	Mobile   string `gorm:"column:EU_MOBILE"`
+	Address  string `gorm:"column:EU_ADDRESS"`
+	Status   string `gorm:"column:EU_STATUS"`
+}
+
+func (User) TableName() string {
+	return "EASYBUY_USER"
+}
